bbdb594657e3fe4e65c291997ea5c36c30d5a01f
DiFFtY <diffty@gmail.com>
2020-01-29 21:57:35 +0100

    Initial commit

---
+++ reconductools-service.py
@@ -0,0 +1,43 @@
+from flask import Flask
+import os
+import json
+
+import vodcutter.vodcutter as cutter
+import gdown
+
+
+app = Flask(__name__)
+
+
+def _load_config():
+    fp = open("config.json", "r")
+    config = json.load(fp)
+    fp.close()
+    return config
+
+
+@app.route('/vodcutter/spreadsheet/<string:spreadsheet_id>/sheet/<int:sheet_id>')
+def vodcutter(spreadsheet_id, sheet_id):
+    config = _load_config()
+
+    creds = cutter.auth()
+    spreadsheet_service = cutter.get_spreadsheet_service(creds)
+    sheet_name = cutter.get_sheet_name_by_id(spreadsheet_service, spreadsheet_id, sheet_id) 
+    vod_target_filename = sheet_name + ".mp4"
+    
+    if config["vod_provider"] == "gdrive":
+        if not os.path.exists(vod_target_filename):
+            print("<i> VOD not existing, downloading")
+            gdrive_link = cutter.get_gdrive_link(creds, spreadsheet_id, sheet_id)
+            video_path = cutter.download(gdrive_link)
+            os.rename(video_path, vod_target_filename)
+        else:
+            print("<i> VOD already exists, skipping download!")
+
+    elif config["vod_provider"] == "file":
+        pass
+
+    cutter.main(vod_target_filename, spreadsheet_id, sheet_id)
+    
+    return ""
+
---
+++ vodcutter/vodcutter.py
@@ -0,0 +1,149 @@
+import pickle
+import os
+import sys
+import datetime
+import re
+import json
+
+import gdown
+
+from googleapiclient.discovery import build
+from google_auth_oauthlib.flow import InstalledAppFlow
+from google.auth.transport.requests import Request
+
+SCOPES = ["https://www.googleapis.com/auth/spreadsheets.readonly"]
+
+
+def auth():
+    # Auth
+    creds = None
+
+    if os.path.exists("creds/token.pickle"):
+        with open("creds/token.pickle", "rb") as token:
+            creds = pickle.load(token)
+
+    if not creds or not creds.valid:
+        if creds and creds.expired and creds.refresh_token:
+            creds.refresh(Request())
+        else:
+            flow = InstalledAppFlow.from_client_secrets_file(
+                "creds/credentials.json", SCOPES
+            )
+
+            creds = flow.run_console()
+        
+        with open("creds/token.pickle", "wb") as token:
+            pickle.dump(creds, token)
+
+    return creds
+
+
+def get_spreadsheet_service(creds):
+    service = build("sheets", "v4", credentials=creds)
+    return service.spreadsheets()
+ 
+
+def get_sheet_name_by_id(spreadsheet_service, spreadsheet_id, sheet_id):
+    spreadsheet = spreadsheet_service.get(
+        spreadsheetId=spreadsheet_id,
+    ).execute()
+    
+    for _sheet in spreadsheet["sheets"]:
+        if str(_sheet["properties"]["sheetId"]) == str(sheet_id):
+            return _sheet["properties"]["title"]
+    else:
+        raise Exception("Can't find sheet with id %s" % sheet_id)
+
+
+def get_info(creds, spreadsheet_id, sheet_id):
+    spreadsheet_service = get_spreadsheet_service(creds) 
+
+    sheet_name = get_sheet_name_by_id(spreadsheet_service, spreadsheet_id, sheet_id)
+
+    spreadsheet = spreadsheet_service.values().get(
+        spreadsheetId=spreadsheet_id,
+        range=sheet_name + "!A3:G12"
+    ).execute()
+    
+    return spreadsheet.get("values", [])
+
+
+def get_gdrive_link(creds, spreadsheet_id, sheet_id):
+    spreadsheet_service = get_spreadsheet_service(creds)
+
+    spreadsheet = spreadsheet_service.get(
+        spreadsheetId=spreadsheet_id,
+    ).execute()
+    
+    sheet_name = get_sheet_name_by_id(spreadsheet_service, spreadsheet_id, sheet_id)
+
+    gdrive_link_range = spreadsheet_service.values().get(
+        spreadsheetId=spreadsheet_id,
+        range=sheet_name + "!K1"
+    ).execute()
+    
+    values = gdrive_link_range.get("values", [])
+    return values[0][0]
+
+
+def make_vod_name(vod_name, title, streamer_name, date, time):
+    date_parsed  = datetime.datetime.strptime(date, "%d/%m/%Y")
+    time_parsed  = datetime.datetime.strptime(time, "%H:%M")
+    new_date = date_parsed.strftime("%Y%m%d")
+    new_time = time_parsed.strftime("%H%M")
+    title = title.strip(" \n\t").replace(" ", "")
+    streamer_name = streamer_name.strip(" \n\t").replace(" ", "")
+    return "_".join([vod_name, new_date, new_time, streamer_name, title])
+
+
+def cut_video(input_video, tc_start, tc_end, output_video):
+    cmd = 'ffmpeg -i "%s" -ss "%s" -to "%s" -c:v copy -c:a copy "%s"' % (input_video, tc_start, tc_end, output_video)
+    os.system(cmd)
+
+
+def download(gdrive_file_url):
+    url_reg = re.search(r"id=([^&]*)", gdrive_file_url)
+    if not url_reg:
+        raise Exception("Invalid Google Drive url: %s" % url_reg)
+    
+    gdrive_file_id = url_reg.group(1)
+    gdrive_file_url = "https://drive.google.com/uc?id=%s" % gdrive_file_id
+    
+    output_file = gdown.download(gdrive_file_url, None, False, None)
+    return output_file
+
+
+def main(input_video, spreadsheet_id, sheet_id):
+    creds = auth()
+    values = get_info(creds, spreadsheet_id, sheet_id)
+
+    spreadsheet_service = get_spreadsheet_service(creds)
+    sheet_name = get_sheet_name_by_id(spreadsheet_service, spreadsheet_id, sheet_id)
+    
+    for v in values:
+        tc_start = v[0]
+        tc_end   = v[1]
+        streamer = v[2]
+        title    = v[3]
+        date     = v[4]
+        time     = v[5]
+
+        vod_name = make_vod_name(
+            sheet_name,
+            title,
+            streamer,
+            date,
+            time,
+        )
+
+        output_video = vod_name + ".mp4"
+
+        cut_video(input_video, tc_start, tc_end, output_video)
+
+
+if __name__ == "__main__":
+    input_video = sys.argv[1]
+    sheet_name = sys.argv[2]
+
+    main(input_video, sheet_name)
+    
